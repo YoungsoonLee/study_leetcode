45953573dfd5705bc34e7548dc3c4fa7634105ed
JayJay <youngtip@gmail.com>
2018-07-27 16:39:48 -0700

    .

---
+++ 0000-4. rest example/gr2/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type User struct {
+	username string `json:"username"`
+	password string `json:"password"`
+}
+
+func main() {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/echo", echo).Methods("GET")
+
+	log.Println("starting a server port 8080")
+	log.Fatalln(http.ListenAndServe(":8080", r))
+}
+
+func echo(w http.ResponseWriter, r *http.Request) {
+
+	log.Println("/echo")
+
+	user := User{}
+
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		log.Println(err)
+	}
+
+	user.password = "1234"
+
+	// return
+	userJSON, err := json.Marshal(user)
+	if err != nil {
+		log.Println("error marshall")
+	}
+
+	w.Header().Set("Content-type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(userJSON)
+}
