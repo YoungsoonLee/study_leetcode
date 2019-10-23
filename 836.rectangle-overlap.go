4ae98e47051d47a91413357433196e0ed09a4b56
Ted Youngsoon Lee <youngtip@gmail.com>
2019-10-23 09:53:44 +0900

    .

---
+++ 836.rectangle-overlap.go
@@ -0,0 +1,68 @@
+/*
+ * @lc app=leetcode id=836 lang=golang
+ *
+ * [836] Rectangle Overlap
+ *
+ * https://leetcode.com/problems/rectangle-overlap/description/
+ *
+ * algorithms
+ * Easy (47.24%)
+ * Likes:    425
+ * Dislikes: 83
+ * Total Accepted:    32.8K
+ * Total Submissions: 69.1K
+ * Testcase Example:  '[0,0,2,2]\n[1,1,3,3]'
+ *
+ * A rectangle is represented as a list [x1, y1, x2, y2], where (x1, y1) are
+ * the coordinates of its bottom-left corner, and (x2, y2) are the coordinates
+ * of its top-right corner.
+ *
+ * Two rectangles overlap if the area of their intersection is positive.  To be
+ * clear, two rectangles that only touch at the corner or edges do not
+ * overlap.
+ *
+ * Given two (axis-aligned) rectangles, return whether they overlap.
+ *
+ * Example 1:
+ *
+ *
+ * Input: rec1 = [0,0,2,2], rec2 = [1,1,3,3]
+ * Output: true
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: rec1 = [0,0,1,1], rec2 = [1,0,2,1]
+ * Output: false
+ *
+ *
+ * Notes:
+ *
+ *
+ * Both rectangles rec1 and rec2 are lists of 4 integers.
+ * All coordinates in rectangles will be between -10^9 and 10^9.
+ *
+ *
+ */
+
+// @lc code=start
+func isRectangleOverlap(rec1 []int, rec2 []int) bool {
+	h1, v1 := projecting(rec1)
+	h2, v2 := projecting(rec2)
+
+	return isLineOverlap(h1, h2) && isLineOverlap(v1, v2)
+}
+
+func projecting(rec []int) (h, v []int) {
+	h = []int{rec[0], rec[2]}
+	v = []int{rec[1], rec[3]}
+	return
+}
+
+func isLineOverlap(a, b []int) bool {
+	return !(a[1] <= b[0] || b[1] <= a[0])
+}
+
+// @lc code=end
+
