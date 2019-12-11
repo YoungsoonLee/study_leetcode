f26b4e378165614c21b5585ad226f23963ebc968
Ted Youngsoon Lee <youngtip@gmail.com>
2019-12-10 21:54:58 -0800

    .

--- 965.univalued-binary-tree.go
+++ 965.univalued-binary-tree.go
@@ -14,36 +14,36 @@
  * Testcase Example:  '[1,1,1,1,1,null,1]'
  *
  * A binary tree is univalued if every node in the tree has the same value.
- * 
+ *
  * Return true if and only if the given tree is univalued.
- * 
- * 
- * 
+ *
+ *
+ *
  * Example 1:
- * 
- * 
+ *
+ *
  * Input: [1,1,1,1,1,null,1]
  * Output: true
- * 
- * 
- * 
+ *
+ *
+ *
  * Example 2:
- * 
- * 
+ *
+ *
  * Input: [2,2,2,5,2]
  * Output: false
- * 
- * 
- * 
- * 
- * 
+ *
+ *
+ *
+ *
+ *
  * Note:
- * 
- * 
+ *
+ *
  * The number of nodes in the given tree will be in the range [1, 100].
  * Each node's value will be an integer in the range [0, 99].
- * 
- * 
+ *
+ *
  */
 
 // @lc code=start
@@ -56,7 +56,16 @@
  * }
  */
 func isUnivalTree(root *TreeNode) bool {
-    
+	if root == nil {
+		return true
+	}
+
+	if (root.Left != nil && root.Val != root.Left.Val) || (root.Right != nil && root.Val != root.Right.Val) {
+		return false
+	}
+
+	return isUnivalTree(root.Left) && isUnivalTree(root.Right)
 }
+
 // @lc code=end
 
