de2419415cb71fe285beca614120a15162130a6d
Ted Youngsoon Lee <youngtip@gmail.com>
2019-12-18 22:33:30 -0800

    .

---
+++ 976.largest-perimeter-triangle.go
@@ -0,0 +1,77 @@
+/*
+ * @lc app=leetcode id=976 lang=golang
+ *
+ * [976] Largest Perimeter Triangle
+ *
+ * https://leetcode.com/problems/largest-perimeter-triangle/description/
+ *
+ * algorithms
+ * Easy (56.91%)
+ * Likes:    241
+ * Dislikes: 27
+ * Total Accepted:    23.8K
+ * Total Submissions: 41.7K
+ * Testcase Example:  '[2,1,2]'
+ *
+ * Given an array A of positive lengths, return the largest perimeter of a
+ * triangle with non-zero area, formed from 3 of these lengths.
+ * 
+ * If it is impossible to form any triangle of non-zero area, return 0.
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: [2,1,2]
+ * Output: 5
+ * 
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: [1,2,1]
+ * Output: 0
+ * 
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: [3,2,3,4]
+ * Output: 10
+ * 
+ * 
+ * 
+ * Example 4:
+ * 
+ * 
+ * Input: [3,6,2,3]
+ * Output: 8
+ * 
+ * 
+ * 
+ * 
+ * Note:
+ * 
+ * 
+ * 3 <= A.length <= 10000
+ * 1 <= A[i] <= 10^6
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+// @lc code=start
+func largestPerimeter(A []int) int {
+    
+}
+// @lc code=end
+
