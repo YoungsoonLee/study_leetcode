28a4612802ff1a46de860d4a1a5aefb71b8a8b6d
Ted Youngsoon Lee <youngtip@gmail.com>
2020-01-29 22:30:12 -0800

    .

--- 1022.sum-of-root-to-leaf-binary-numbers.go
+++ 1022.sum-of-root-to-leaf-binary-numbers.go
@@ -17,34 +17,34 @@
  * represents a binary number starting with the most significant bit.  For
  * example, if the path is 0 -> 1 -> 1 -> 0 -> 1, then this could represent
  * 01101 in binary, which is 13.
- * 
+ *
  * For all leaves in the tree, consider the numbers represented by the path
  * from the root to that leaf.
- * 
+ *
  * Return the sum of these numbers.
- * 
- * 
- * 
+ *
+ *
+ *
  * Example 1:
- * 
- * 
- * 
- * 
+ *
+ *
+ *
+ *
  * Input: [1,0,1,0,1,0,1]
  * Output: 22
  * Explanation: (100) + (101) + (110) + (111) = 4 + 5 + 6 + 7 = 22
- * 
- * 
- * 
- * 
+ *
+ *
+ *
+ *
  * Note:
- * 
- * 
+ *
+ *
  * The number of nodes in the tree is between 1 and 1000.
  * node.val is 0 or 1.
  * The answer will not exceed 2^31 - 1.
- * 
- * 
+ *
+ *
  */
 
 // @lc code=start
@@ -57,7 +57,24 @@
  * }
  */
 func sumRootToLeaf(root *TreeNode) int {
-    
+	return dfs(root, 0)
 }
+
+func dfs(root *TreeNode, b int) int {
+	if root == nil {
+		return 0
+	}
+
+	b = b*2 + root.Val
+
+	// end condition
+	if root.Left == root.Right { // both nil
+		return b
+	}
+
+	return dfs(root.Left, b) + dfs(root.Right, b)
+
+}
+
 // @lc code=end
 
