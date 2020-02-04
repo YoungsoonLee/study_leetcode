f1024aef72034de7b04ee3d51d33eb970bf70bda
ted <ted@lbcstudios.ca>
2020-02-04 14:35:01 -0800

    .

---
+++ 1051.height-checker.go
@@ -0,0 +1,42 @@
+/*
+ * @lc app=leetcode id=1051 lang=golang
+ *
+ * [1051] Height Checker
+ *
+ * https://leetcode.com/problems/height-checker/description/
+ *
+ * algorithms
+ * Easy (68.39%)
+ * Likes:    140
+ * Dislikes: 975
+ * Total Accepted:    36.9K
+ * Total Submissions: 53.9K
+ * Testcase Example:  '[1,1,4,2,1,3]'
+ *
+ * Students are asked to stand in non-decreasing order of heights for an annual
+ * photo.
+ * 
+ * Return the minimum number of students that must move in order for all
+ * students to be standing in non-decreasing order of height.
+ * 
+ * 
+ * Example 1:
+ * Input: heights = [1,1,4,2,1,3]
+ * Output: 3
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 1 <= heights.length <= 100
+ * 1 <= heights[i] <= 100
+ * 
+ * 
+ */
+
+// @lc code=start
+func heightChecker(heights []int) int {
+    
+}
+// @lc code=end
+
