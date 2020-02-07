d42fd53f934d29b981be9c32b94591d5f6656381
Ted Youngsoon Lee <youngtip@gmail.com>
2020-02-07 15:45:02 -0800

    .

--- 1078.occurrences-after-bigram.go
+++ 1078.occurrences-after-bigram.go
@@ -1,3 +1,8 @@
+import (
+	"fmt"
+	"strings"
+)
+
 /*
  * @lc app=leetcode id=1078 lang=golang
  *
@@ -16,46 +21,75 @@
  * Given words first and second, consider occurrences in some text of the form
  * "first second third", where second comes immediately after first, and third
  * comes immediately after second.
- * 
+ *
  * For each such occurrence, add "third" to the answer, and return the
  * answer.
- * 
- * 
- * 
+ *
+ *
+ *
  * Example 1:
- * 
- * 
+ *
+ *
  * Input: text = "alice is a good girl she is a good student", first = "a",
  * second = "good"
  * Output: ["girl","student"]
- * 
- * 
- * 
+ *
+ *
+ *
  * Example 2:
- * 
- * 
+ *
+ *
  * Input: text = "we will we will rock you", first = "we", second = "will"
  * Output: ["we","rock"]
- * 
- * 
- * 
- * 
+ *
+ *
+ *
+ *
  * Note:
- * 
- * 
+ *
+ *
  * 1 <= text.length <= 1000
  * text consists of space separated words, where each word consists of
  * lowercase English letters.
  * 1 <= first.length, second.length <= 10
  * first and second consist of lowercase English letters.
- * 
- * 
- * 
+ *
+ *
+ *
  */
 
 // @lc code=start
 func findOcurrences(text string, first string, second string) []string {
-    
+	sp := strings.Split(text, " ")
+	tp := sp
+	//fmt.Println(sp)
+
+	res := make([]string, 0)
+
+	for i, s := range sp {
+		//fmt.Println(tp)
+		if s == first {
+			fmt.Println(i)
+			tp = tp[i+2:]
+			fmt.Println(tp, i)
+			res = append(res, tp[0])
+			continue
+		}
+
+		if s == second {
+			fmt.Println(i)
+			tp = tp[i+2:]
+			fmt.Println(tp, i)
+			res = append(res, tp[0])
+			continue
+		}
+
+	}
+
+	//fmt.Println(tp)
+
+	return res
 }
+
 // @lc code=end
 
