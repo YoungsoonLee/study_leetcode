bd32c1042c49d3104268a5ca37391330c3f3adba
ted <ted@lbcstudios.ca>
2020-02-26 16:38:03 -0800

    .

--- 1160.find-words-that-can-be-formed-by-characters.go
+++ 1160.find-words-that-can-be-formed-by-characters.go
@@ -57,31 +57,36 @@ import "fmt"
 
 // @lc code=start
 func countCharacters(words []string, chars string) int {
-	m := make(map[rune]int)
-
-	for _, c := range chars {
-		m[c]++
-	}
-
-	fmt.Println(m)
+	char := parse(chars)
+	fmt.Println(char)
 
 	res := 0
-	for _, w := range words {
-		lc := len(w)
-
-		for _, c := range w {
-			if n, ok := m[c]; ok && n > 0 {
-				lc--
-			}
-		}
-
-		if lc == 0 {
-			res += len(w)
-		}
+	for _, word := range words {
+		res += count(char, word)
 	}
 
-	fmt.Println(res)
+	return res
+}
 
+func parse(s string) []int {
+	res := make([]int, 26)
+	for _, r := range s {
+		res[r-'a']++
+	}
+	return res
+}
+
+func count(char []int, word string) int {
+	res := 0
+	w := make([]int, 26)
+	for _, r := range word {
+		b := r - 'a'
+		w[b]++
+		if w[b] > char[b] {
+			return 0
+		}
+		res++
+	}
 	return res
 }
 
