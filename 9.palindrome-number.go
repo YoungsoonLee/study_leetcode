bad3df98931551178561bdfbc5328ccfe55dd703
ted <ted@lbcstudios.ca>
2020-03-18 14:18:38 -0700

    .

--- 9.palindrome-number.go
+++ 9.palindrome-number.go
@@ -1,3 +1,8 @@
+import (
+	"fmt"
+	"strconv"
+)
+
 /*
  * @lc app=leetcode id=9 lang=golang
  *
@@ -15,41 +20,59 @@
  *
  * Determine whether an integer is a palindrome. An integer is a palindrome
  * when it reads the same backward as forward.
- * 
+ *
  * Example 1:
- * 
- * 
+ *
+ *
  * Input: 121
  * Output: true
- * 
- * 
+ *
+ *
  * Example 2:
- * 
- * 
+ *
+ *
  * Input: -121
  * Output: false
  * Explanation: From left to right, it reads -121. From right to left, it
  * becomes 121-. Therefore it is not a palindrome.
- * 
- * 
+ *
+ *
  * Example 3:
- * 
- * 
+ *
+ *
  * Input: 10
  * Output: false
  * Explanation: Reads 01 from right to left. Therefore it is not a
  * palindrome.
- * 
- * 
+ *
+ *
  * Follow up:
- * 
+ *
  * Coud you solve it without converting the integer to a string?
- * 
+ *
  */
 
 // @lc code=start
 func isPalindrome(x int) bool {
-    
+	if x < 0 {
+		return false
+	}
+
+	sx := strconv.Itoa(x)
+
+	i, j := 0, len(sx)-1
+	fmt.Println(sx, i, j)
+	for i < j {
+		if sx[i] != sx[j] {
+			return false
+		}
+		i++
+		j--
+	}
+
+	return true
+
 }
+
 // @lc code=end
 
