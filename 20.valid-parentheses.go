6ee8a2df8b37492936737c2a07ce1342186c492c
Ted Youngsoon Lee <youngtip@gmail.com>
2020-03-25 23:28:23 -0700

    .

---
+++ 20.valid-parentheses.go
@@ -0,0 +1,70 @@
+/*
+ * @lc app=leetcode id=20 lang=golang
+ *
+ * [20] Valid Parentheses
+ *
+ * https://leetcode.com/problems/valid-parentheses/description/
+ *
+ * algorithms
+ * Easy (37.85%)
+ * Likes:    4327
+ * Dislikes: 202
+ * Total Accepted:    890.5K
+ * Total Submissions: 2.3M
+ * Testcase Example:  '"()"'
+ *
+ * Given a string containing just the characters '(', ')', '{', '}', '[' and
+ * ']', determine if the input string is valid.
+ * 
+ * An input string is valid if:
+ * 
+ * 
+ * Open brackets must be closed by the same type of brackets.
+ * Open brackets must be closed in the correct order.
+ * 
+ * 
+ * Note that an empty string is also considered valid.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: "()"
+ * Output: true
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: "()[]{}"
+ * Output: true
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: "(]"
+ * Output: false
+ * 
+ * 
+ * Example 4:
+ * 
+ * 
+ * Input: "([)]"
+ * Output: false
+ * 
+ * 
+ * Example 5:
+ * 
+ * 
+ * Input: "{[]}"
+ * Output: true
+ * 
+ * 
+ */
+
+// @lc code=start
+func isValid(s string) bool {
+    
+}
+// @lc code=end
+
