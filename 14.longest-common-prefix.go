67f19db1b40b0fe530083346f459317fd17ba196
Ted Youngsoon Lee <youngtip@gmail.com>
2020-03-27 14:57:56 -0700

    .

--- 14.longest-common-prefix.go
+++ 14.longest-common-prefix.go
@@ -1,3 +1,5 @@
+import "fmt"
+
 /*
  * @lc app=leetcode id=14 lang=golang
  *
@@ -15,33 +17,62 @@
  *
  * Write a function to find the longest common prefix string amongst an array
  * of strings.
- * 
+ *
  * If there is no common prefix, return an empty string "".
- * 
+ *
  * Example 1:
- * 
- * 
+ *
+ *
  * Input: ["flower","flow","flight"]
  * Output: "fl"
- * 
- * 
+ *
+ *
  * Example 2:
- * 
- * 
+ *
+ *
  * Input: ["dog","racecar","car"]
  * Output: ""
  * Explanation: There is no common prefix among the input strings.
- * 
- * 
+ *
+ *
  * Note:
- * 
+ *
  * All given inputs are in lowercase letters a-z.
- * 
+ *
  */
 
 // @lc code=start
 func longestCommonPrefix(strs []string) string {
-    
+	short := shortest(strs)
+	fmt.Println(short)
+
+	for i, r := range short {
+		for j := 0; j < len(strs); j++ {
+			if strs[j][i] != byte(r) {
+				fmt.Println(strs[j][:i])
+				return strs[j][:i]
+			}
+		}
+	}
+
+	return short
+
 }
+
+func shortest(strs []string) string {
+	if len(strs) == 0 {
+		return ""
+	}
+
+	res := strs[0]
+	for _, s := range strs {
+		if len(res) > len(s) {
+			res = s // 작은 걸 세팅.ㅠ
+		}
+	}
+
+	return res
+}
+
 // @lc code=end
 
