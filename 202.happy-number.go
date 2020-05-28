31edf926c0deb403e2a07d25b46babcbf6c94c27
Ted Youngsoon Lee <youngtip@gmail.com>
2020-05-27 22:06:02 -0700

    .

--- 202.happy-number.go
+++ 202.happy-number.go
@@ -38,8 +38,32 @@
  */
 
 // @lc code=start
+
 func isHappy(n int) bool {
-    
+	slow, fast := n, trans(n)
+	fmt.Println(slow, fast)
+
+	for slow != fast {
+		slow = trans(slow)
+		fast = trans(trans(fast))
+	}
+
+	if slow == 1 {
+		return true
+	}
+
+	return false
+
 }
+
+func trans(n int) int {
+	res := 0
+	for n != 0 {
+		res += (n%10) * (n%10)
+		n /= 10
+	}
+	return res
+}
+
 // @lc code=end
 
