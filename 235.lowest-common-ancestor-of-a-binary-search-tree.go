734acb4ab18c753d8757d13221790a32e796d95d
Ted Youngsoon Lee <youngtip@gmail.com>
2020-07-29 23:29:10 -0700

    .

---
+++ 235.lowest-common-ancestor-of-a-binary-search-tree.go
@@ -0,0 +1,78 @@
+/*
+ * @lc app=leetcode id=235 lang=golang
+ *
+ * [235] Lowest Common Ancestor of a Binary Search Tree
+ *
+ * https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/description/
+ *
+ * algorithms
+ * Easy (47.53%)
+ * Likes:    2128
+ * Dislikes: 106
+ * Total Accepted:    409.4K
+ * Total Submissions: 822.7K
+ * Testcase Example:  '[6,2,8,0,4,7,9,null,null,3,5]\n2\n8'
+ *
+ * Given a binary search tree (BST), find the lowest common ancestor (LCA) of
+ * two given nodes in the BST.
+ * 
+ * According to the definition of LCA on Wikipedia: “The lowest common ancestor
+ * is defined between two nodes p and q as the lowest node in T that has both p
+ * and q as descendants (where we allow a node to be a descendant of itself).”
+ * 
+ * Given binary search tree:  root = [6,2,8,0,4,7,9,null,null,3,5]
+ * 
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 8
+ * Output: 6
+ * Explanation: The LCA of nodes 2 and 8 is 6.
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 4
+ * Output: 2
+ * Explanation: The LCA of nodes 2 and 4 is 2, since a node can be a descendant
+ * of itself according to the LCA definition.
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * All of the nodes' values will be unique.
+ * p and q are different and both values will exist in the BST.
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val   int
+ *     Left  *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	return helper(root, p.Val, q.Val)
+}
+
+func helper(root *TreeNode, p,q int) *TreeNode {
+	r := root.Val
+	if p < r && q < r {
+		return helper(root.Left, p, q)
+	}else if r < p && r < q {
+		return helper(root.Right, p, q)
+	}
+	return root
+}
+// @lc code=end
+
